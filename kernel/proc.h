54c5f04b177e0d55c9913d455f699e5931169e6b
joacomm1 <142599050+joacomm1@users.noreply.github.com>
2023-11-23 10:30:27 -0300

    Update proc.h

--- kernel/proc.h
+++ kernel/proc.h
@@ -104,5 +104,5 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  int priority;
+  int priority; // setting priority from (0-20)
 };
